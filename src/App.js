8ca44cfa1102d4cd7dc0b220bc0cfa6bb0b7aa58
Mike <michaelmclean08@gmail.com>
2017-02-25 14:04:09 -0500

    implment GET to react movies sample api

--- src/App.js
+++ src/App.js
@@ -2,17 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Movie extends Component {
+  render(){
+    return (
+      <div>{this.props.title}</div>
+    );
+  }
+}
+
+class MoviePane extends Component {
   render() {
+    const movies = this.props.movies ?  this.props.movies.map ((movie, i) => {
+        return (<Movie key={i} title={movie.title}></Movie>);
+    }) : "no movies";
+    return (
+     <div>
+       {movies}
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {movies: null}
+  } 
+  
+  getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.movies;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  async componentDidMount() {
+    this.setState({movies: await this.getMoviesFromApiAsync()});
+  }
+
+
+ render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Movies</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <MoviePane
+            movies={this.state.movies}/>
       </div>
     );
   }
